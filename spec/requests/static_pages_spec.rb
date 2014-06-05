c6ed1773b12c288d1a26d6d599ed64771650858b
hannahbarudin <hannah@adaptly.com>
2014-06-05 14:39:35 -0400

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,17 +5,17 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Home')
     end
   
     it "should have base title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title('SampleApp')
     end
 
     it "should not have a custom title" do 
-        visit '/static_pages/home'
+        visit root_path
         expect(page).not_to have_title('| Home')
       end
   end
@@ -24,12 +24,12 @@ describe "Static pages" do
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title('Help')
     end
   end
@@ -37,12 +37,12 @@ describe "Static pages" do
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
 
     it "should have the title 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title('About')
     end
   end
@@ -50,12 +50,12 @@ describe "Static pages" do
   describe "Contact page" do
 
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact')
     end
 
     it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title('Contact')
     end
   end
